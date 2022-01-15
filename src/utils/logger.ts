8522c517fd682a3b7623c13183ef71d1b95d41e7
JoaoDuarte10 <joaoduarte147@gmail.com>
2022-01-15 14:11:36 -0300

    feat: add factory logger and dependences logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,33 @@
+import { createLogger, format, transports } from 'winston'
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(({ timestamp, level, message }) => {
+    return `Date: [${timestamp}] Level: [${level}] Message: { ${message} }`;
+});
+
+const logger = createLogger({
+    format: combine(
+        format.splat(),
+        format.simple(),
+        timestamp(),
+        myFormat,
+    ),
+    transports: [
+        new transports.File({
+            filename: './logs/errors.log',
+            level: 'error',
+        }),
+        new transports.File({
+            filename: './logs/info.log',
+            level: 'info',
+        })
+    ],
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new transports.Console({
+        format: format.simple(),
+    }));
+}
+
+export { logger }
