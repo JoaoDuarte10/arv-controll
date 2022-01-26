9a1dbd340bc7628c31b68ed9895563bba1cd0532
JoaoDuarte10 <joaoduarte147@gmail.com>
2022-01-25 23:37:29 -0300

    adjusts in errors return client does exist

--- src/useCases/clientUseCase.ts
+++ src/useCases/clientUseCase.ts
@@ -5,13 +5,16 @@ class ClientUseCase {
     constructor(private clientRepository: ClientRepository) { }
 
     async newClient({ id_user, name, email, phone }: IClient): Promise<void> {
-        try {
+        if (email) {
             const findClient = await this.clientRepository.findByEmail(id_user, email);
 
             if (findClient) throw {
                 type: "Client already exists",
                 message: "Client does exist"
             }
+        }
+
+        try {
             await this.clientRepository.newClient({ id_user, name, email, phone })
 
         } catch (error) {
@@ -20,13 +23,13 @@ class ClientUseCase {
     }
 
     async updateClient({ id_user, id, name, email, phone }: IClient): Promise<IClient[]> {
-        try {
-            const findClient = await this.clientRepository.findClient(id_user, id)
-            if (!findClient) throw {
-                type: "Client already not exists",
-                message: "Client does not exist"
-            }
+        const findClient = await this.clientRepository.findClient(id_user, id)
+        if (!findClient) throw {
+            type: "Client already not exists",
+            message: "Client does not exist"
+        }
 
+        try {
             const updateClient = await this.clientRepository.updateClient({ id_user, id, name, email, phone })
             return updateClient
 
@@ -56,20 +59,20 @@ class ClientUseCase {
     }
 
     async deleteClient(id_user: string, id: string): Promise<void> {
-        try {
-            const findClient = await this.clientRepository.findClient(id_user, id)
+        const findClient = await this.clientRepository.findClient(id_user, id)
 
-            if (!findClient) throw {
-                type: "Client already not exists",
-                message: "Client does not exist"
-            }
+        if (!findClient) throw {
+            type: "Client already not exists",
+            message: "Client does not exist"
+        }
+
+        try {
             const deleteClient = await this.clientRepository.deleteClient(id_user, id)
 
         } catch (error) {
             logger.error(`Error in ClientUseCase in function deleteClient: ${error.message}`)
         }
     }
-
 }
 
 export { ClientUseCase }
