489048416ed62dc4df5eb2a4f2a1c588c9c3dfdb
JoaoDuarte10 <joaoduarte147@gmail.com>
2022-02-03 22:59:27 -0300

    adjusts in front salao

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,13 @@ app.use(router);
 
 app.use(express.static(path.join(__dirname, '../build')))
 
+app.get('/schedule', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+app.get('/pos-schedule', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'))
 })
