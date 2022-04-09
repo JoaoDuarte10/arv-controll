fa0d7993216481f865f616e958e0a4567fcacfdc
joaoduarte10 <joaoduarte7341@gmail.com>
2022-04-09 02:43:44 +0000

    refactor: update scripts shell

--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -4,4 +4,4 @@ echo -e " \033[0;32m Destruindo todos os containeres existentes \033[0m"
 docker-compose -f down -v
 
 echo -e " \033[0;32m Excluindo a pasta dist/ \033[0m"
-rm -r ../dist/
+rm -r dist/
--- scripts/update-app.sh
+++ scripts/update-app.sh
@@ -2,5 +2,11 @@
 
 echo -e " \033[0;32m Atualizando o projeto \033[0m"
 git pull origin master
+
+sleep 2
+
 joaoduarte10
+
+sleep 2
+
 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
