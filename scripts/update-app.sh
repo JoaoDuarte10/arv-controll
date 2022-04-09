0e438b548a1920a2ba9bdebd7e3ad6cd05f3e66b
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-04-09 10:54:12 -0300

    refactor: update commands the app

--- scripts/update-app.sh
+++ scripts/update-app.sh
@@ -2,14 +2,9 @@
 
 echo -e " \033[0;32m Atualizando o Projeto \033[0m" "\n"
 
-echo -e " \033[0;32m ============================ DEVELOP ======================================= \033[0m" "\n"
-
-git checkout develop
-git pull origin develop
-
-echo -e "\n" " \033[0;32m ============================ MASTER =================================== \033[0m" "\n"
-
 git checkout master
 git pull origin master
 
-echo -e "\n" " \033[0;32m =========================== END ====================================== \033[0m" "\n"
+git checkout develop
+git pull origin develop
+git merge master
