6087c0529b74fae297b2cbe0dda70f1f4921a7e5
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-04-08 21:50:01 -0300

    feat: adjust route /segments when reload page

--- src/routes.ts
+++ src/routes.ts
@@ -7,83 +7,83 @@ const router = Router();
 
 router.use(cors());
 
-router.post('/login', (req, res) => {
+router.post('/api/login', (req, res) => {
     return loginController.findLogin(req, res);
 })
 
-router.post('/new-sale', (req, res) => {
+router.post('/api/new-sale', (req, res) => {
     return salesController.saveSales(req, res);
 })
 
-router.post('/sales-today', (req, res) => {
+router.post('/api/sales-today', (req, res) => {
     return salesController.findSalesByDate(req, res);
 })
 
-router.post('/sales', (req, res) => {
+router.post('/api/sales', (req, res) => {
     return salesController.findSalesByPeriod(req, res);
 })
 
-router.post('/sales-period-clients', (req, res) => {
+router.post('/api/sales-period-clients', (req, res) => {
     return salesController.findSalesByClientsForPeriod(req, res);
 })
 
-router.post('/sales-clients', (req, res) => {
+router.post('/api/sales-clients', (req, res) => {
     return salesController.findSalesForClient(req, res);
 })
 
-router.post('/schedule', (req, res) => {
+router.post('/api/schedule', (req, res) => {
     return scheduleController.findScheduleByDate(req, res);
 })
 
-router.post('/new-schedule', (req, res) => {
+router.post('/api/new-schedule', (req, res) => {
     return scheduleController.saveSchedule(req, res);
 })
 
-router.put('/update-schedule', (req, res) => {
+router.put('/api/update-schedule', (req, res) => {
     return scheduleController.updateSchedule(req, res);
 })
 
-router.delete('/delete-schedule', (req, res) => {
+router.delete('/api/delete-schedule', (req, res) => {
     return scheduleController.deleteSchedule(req, res);
 })
 
-router.post('/new-client', (req, res) => {
+router.post('/api/new-client', (req, res) => {
     return clientController.newClient(req, res);
 })
 
-router.post('/update-client', (req, res) => {
+router.post('/api/update-client', (req, res) => {
     return clientController.updateClient(req, res);
 })
 
-router.post('/find-all-client', (req, res) => {
+router.post('/api/find-all-client', (req, res) => {
     return clientController.findAllClients(req, res);
 })
 
-router.post('/find-client', (req, res) => {
+router.post('/api/find-client', (req, res) => {
     return clientController.findClient(req, res);
 })
 
-router.post('/find-client-segment', (req, res) => {
+router.post('/api/find-client-segment', (req, res) => {
     return clientController.findClientBySegment(req, res);
 })
 
-router.post('/delete-client', (req, res) => {
+router.post('/api/delete-client', (req, res) => {
     return clientController.deleteClient(req, res);
 })
 
-router.get('/segments', (req, res) => {
+router.get('/api/segments', (req, res) => {
     return segmentController.findSegment(req, res);
 })
 
-router.post('/new-schedule-client', (req, res) => {
+router.post('/api/new-schedule-client', (req, res) => {
     return scheduleClientController.saveScheduleClients(req, res);
 })
 
-router.post('/find-schedule-client', (req, res) => {
+router.post('/api/find-schedule-client', (req, res) => {
     return scheduleClientController.findAllScheduleClients(req, res);
 })
 
-router.delete('/delete-schedule-client', (req, res) => {
+router.delete('/api/delete-schedule-client', (req, res) => {
     return scheduleClientController.deleteSchedule(req, res);
 })
 
