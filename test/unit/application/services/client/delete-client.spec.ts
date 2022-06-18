b57aea7243ee32cba423f78d9bf22ee57bb6503a
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-06-18 11:31:24 -0300

    test: ensure DeleteClientService return error if client not exist

--- test/unit/application/services/client/delete-client.spec.ts
+++ test/unit/application/services/client/delete-client.spec.ts
@@ -31,4 +31,17 @@ describe('Create Client Service', () => {
     expect(clientRepositorySpy).toHaveBeenCalledTimes(1);
     expect(loggerErrorSpy).toHaveBeenCalledTimes(0);
   });
+
+  it('should return error with client not exist', async () => {
+    jest.spyOn(makeClientRepository, 'find').mockImplementationOnce(() => {
+      new Promise((resolve, reject) => resolve(null));
+    });
+    try {
+      await clientService.execute('123', '123');
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+    expect(clientRepositorySpy).toHaveBeenCalledTimes(0);
+    expect(loggerErrorSpy).toHaveBeenCalledTimes(0);
+  });
 });
