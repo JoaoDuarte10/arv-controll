ed34f5fcc2ac0f1e58c32b444394934ca08d2c0c
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-06-18 13:12:47 -0300

    test: ensure NotificationError add a new error

---
+++ test/unit/application/notification/notification-error.spec.ts
@@ -0,0 +1,19 @@
+import {
+  NotificationError,
+  NotificationErrorProps,
+} from '../../../../src/application/notification/notification-error';
+describe('Notificatio Error', () => {
+  it('should add a new error', () => {
+    const sut = new NotificationError();
+    const error: NotificationErrorProps = {
+      type: 'InvalidCredentials',
+      message: 'Invalid Credentials',
+    };
+
+    sut.addError(error);
+
+    expect(sut.hasErrors()).toBe(true);
+    expect(sut.getErros()[0]).toStrictEqual(error);
+    expect(sut.getErros().length).toBe(1);
+  });
+});
