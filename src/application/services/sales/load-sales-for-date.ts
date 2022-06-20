b183119a09ad83b2838057c37affdd6f2c2ccbdc
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-06-20 20:01:36 -0300

    feat: ensure LoadSalesForDate logger with error in find sales

--- src/application/services/sales/load-sales-for-date.ts
+++ src/application/services/sales/load-sales-for-date.ts
@@ -1,23 +1,29 @@
 import { Sales } from 'src/domain/entities/sales';
 import { LoadSalesForDate } from '../../../domain/usecases/sales/load-sales-for-date';
 import { SalesRepository } from '../../../domain/repository';
+import { ILogger } from '../../../infrastructure/utils/logger';
 
 export class LoadSalesForDateService implements LoadSalesForDate {
-  constructor(private readonly salesRepository: SalesRepository) {}
+  constructor(
+    private readonly salesRepository: SalesRepository,
+    private readonly logger: ILogger,
+  ) {}
 
   async execute(id_user: string, date: string): Promise<Sales[]> {
-    try {
-      if (new Date(date) > new Date()) {
-        throw {
-          type: 'inputs_invalids',
-          message: 'Invalid Date',
-        };
-      }
+    if (new Date(date) > new Date()) {
+      throw {
+        type: 'inputs_invalids',
+        message: 'Invalid Date',
+      };
+    }
 
+    try {
       const result = await this.salesRepository.findByDate(id_user, date);
       return result;
     } catch (error) {
-      throw new Error(error.message);
+      this.logger.error(
+        `Error SalesUseCase function findSalesByDate: ${error.message}`,
+      );
     }
   }
 }
