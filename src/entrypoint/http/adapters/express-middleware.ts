146ea432d0f9172caf864f2d08b7403df1d15b8b
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-09-07 10:21:14 -0300

    refactor: validate instanceof error in response error in adapteMiddleware

--- src/entrypoint/http/adapters/express-middleware.ts
+++ src/entrypoint/http/adapters/express-middleware.ts
@@ -1,6 +1,8 @@
 import { Middleware } from '../../contracts/middleware';
 import { NextFunction, Request, Response } from 'express';
 import { HTTP_STATUS_CODE } from '../utils/http-status-code';
+import { UnauthorizedException } from '../exceptions/Unauthorized';
+import { InvalidTokenException } from '../exceptions/invalid-token';
 
 export const adapteMiddleware = (middleware: Middleware) => {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -8,7 +10,22 @@ export const adapteMiddleware = (middleware: Middleware) => {
       middleware.handle(req);
       return next();
     } catch (error) {
-      return res.status(HTTP_STATUS_CODE.UNAUTHORIZED).send();
+      if (error instanceof UnauthorizedException) {
+        return res.status(HTTP_STATUS_CODE.UNAUTHORIZED).json({
+          type: 'unauthorized',
+          message: 'Invalid credentials',
+        });
+      } else if (error instanceof InvalidTokenException) {
+        return res.status(HTTP_STATUS_CODE.UNAUTHORIZED).json({
+          type: 'unauthorized',
+          message: 'Invalid token',
+        });
+      } else {
+        return res.status(HTTP_STATUS_CODE.SERVER_ERROR).json({
+          type: 'serverError',
+          message: 'Internal server error',
+        });
+      }
     }
   };
 };
