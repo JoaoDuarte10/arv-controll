5acbdb74b4052a079bf4ae65dbad275088d4c4fd
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-09-14 20:16:33 -0300

    test: add tests in finish schedule application

--- test/unit/application/services/schedule/finish-schedule.spec.ts
+++ test/unit/application/services/schedule/finish-schedule.spec.ts
@@ -1,4 +1,5 @@
 import { FinishScheduleService } from '../../../../../src/application/services/schedule/finish-schedule';
+import { TYPE_NOT_EXISTS } from '../../../../../src/application/utils/type-errors';
 
 describe('Finish Schedule Service', () => {
   let sut = {} as FinishScheduleService;
@@ -7,10 +8,15 @@ describe('Finish Schedule Service', () => {
     update: () => void;
     delete: () => void;
   };
-  let scheduleRepositorySpy;
-  let salesRepositoryMake: { create: () => void };
+  let scheduleRepositorySpy: {
+    delete: jest.SpyInstance;
+    findById: jest.SpyInstance;
+    update: jest.SpyInstance;
+  };
   let clientHistoryRepositoryMake: { save: () => void };
+  let clientHistoryRepositorySpy: { save: jest.SpyInstance };
   let logger: { error: () => void };
+  let loggerSpy: { error: jest.SpyInstance };
 
   let paramsMock: { id_user: string; id: string };
 
@@ -26,7 +32,6 @@ describe('Finish Schedule Service', () => {
             procedure: 'Procedure',
             date: '2022-08-06',
             time: '13:00',
-            price: 'R$15,00',
             phone: '(35) 9 9999-9999',
             isDefeated: false,
             pacote: false,
@@ -38,14 +43,19 @@ describe('Finish Schedule Service', () => {
       update: () => jest.fn(),
       delete: () => jest.fn(),
     };
-    scheduleRepositorySpy = jest.spyOn(scheduleRepositoryMake, 'delete');
-    salesRepositoryMake = {
-      create: () => jest.fn(),
+    scheduleRepositorySpy = {
+      delete: jest.spyOn(scheduleRepositoryMake, 'delete'),
+      findById: jest.spyOn(scheduleRepositoryMake, 'findById'),
+      update: jest.spyOn(scheduleRepositoryMake, 'update'),
     };
     clientHistoryRepositoryMake = {
       save: () => jest.fn(),
     };
+    clientHistoryRepositorySpy = {
+      save: jest.spyOn(clientHistoryRepositoryMake, 'save'),
+    };
     logger = { error: () => jest.fn() };
+    loggerSpy = { error: jest.spyOn(logger, 'error') };
     sut = new FinishScheduleService(
       scheduleRepositoryMake as any,
       clientHistoryRepositoryMake as any,
@@ -57,9 +67,63 @@ describe('Finish Schedule Service', () => {
     };
   });
 
-  it('should create a new sales', async () => {
+  it('should create a new history', async () => {
     await sut.execute(paramsMock);
 
-    expect(scheduleRepositorySpy).toHaveBeenCalledTimes(1);
+    expect(scheduleRepositorySpy.delete).toHaveBeenCalledTimes(1);
+    expect(clientHistoryRepositorySpy.save).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return new error when schedule not exist', async () => {
+    scheduleRepositoryMake.findById = () => {
+      return new Promise((resolve, reject) => {
+        resolve(null);
+      });
+    };
+
+    try {
+      await sut.execute(paramsMock);
+    } catch (error) {
+      expect(error).toBeDefined();
+      expect(error.type).toBe(TYPE_NOT_EXISTS);
+    }
+  });
+
+  it('should update schedule when is invalid for finish', async () => {
+    scheduleRepositoryMake.findById = () => {
+      return new Promise((resolve, reject) => {
+        resolve({
+          id: '123',
+          id_user: '123',
+          idSchedule: '123',
+          client: 'Client',
+          procedure: 'Procedure',
+          date: '2022-08-06',
+          time: '13:00',
+          phone: '(35) 9 9999-9999',
+          isDefeated: false,
+          pacote: true,
+          qtdAtendimento: 0,
+          qtdTotalAtendimento: 4,
+        });
+      });
+    };
+
+    await sut.execute(paramsMock);
+
+    expect(scheduleRepositorySpy.delete).toHaveBeenCalledTimes(0);
+    expect(scheduleRepositorySpy.update).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should logger when exception occured', async () => {
+    jest.spyOn(scheduleRepositoryMake, 'update').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    try {
+      await sut.execute(paramsMock);
+    } catch (error) {
+      expect(loggerSpy.error).toHaveBeenCalledTimes(1);
+    }
   });
 });
